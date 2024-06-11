1a24adcc3e8245d6317f3e1c98edc557638284d7
wuhai <wh@s-track.cn>
2024-06-11 10:34:46 +0800

    chore: init project

---
+++ CMake/wxWidgets.cmake
@@ -0,0 +1,16 @@
+﻿set(packet_name "wxWidgets-3.2.5")
+
+execute_process(COMMAND ${CMAKE_COMMAND} -E make_directory ${packet_name}
+                WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/ThirdParty)
+if(NOT EXISTS ${CMAKE_SOURCE_DIR}/ThirdParty/${packet_name}/CMakeLists.txt)
+  execute_process(
+    COMMAND ${CMAKE_COMMAND} -E tar xzf ${CMAKE_SOURCE_DIR}/ThirdParty/${packet_name}.7z
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/ThirdParty/${packet_name})
+endif()
+
+add_subdirectory(${CMAKE_SOURCE_DIR}/ThirdParty/${packet_name})
+include_directories(${CMAKE_SOURCE_DIR}/ThirdParty/${packet_name}/include)
+
+set(include_temp ${CMAKE_BINARY_DIR}/ThirdParty/${packet_name}/lib/vc_lib/mswu)
+message(STATUS "include_temp:${include_temp}")
+include_directories(${include_temp})
