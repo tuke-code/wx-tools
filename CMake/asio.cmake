1b0fe9bac181e052cf32ffb4944a712acb0baf68
x-tools-author <x-tools@outlook.com>
2024-06-13 13:52:32 +0800

    chore: import the file to project

---
+++ CMake/asio.cmake
@@ -0,0 +1,9 @@
+﻿set(packet_name "asio-asio-1-30-2")
+
+if(NOT EXISTS ${CMAKE_SOURCE_DIR}/ThirdParty/${packet_name})
+  execute_process(
+    COMMAND ${CMAKE_COMMAND} -E tar xzf ${CMAKE_SOURCE_DIR}/ThirdParty/${packet_name}.zip
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/ThirdParty)
+endif()
+
+include_directories(${CMAKE_SOURCE_DIR}/ThirdParty/${packet_name}/asio/include)
