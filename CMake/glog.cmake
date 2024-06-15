fcae4a8fc8edef64991522c83d5c6d1c76e1aec5
x-tools-author <x-tools@outlook.com>
2024-06-15 17:24:15 +0800

    chore: backup code

---
+++ CMake/glog.cmake
@@ -0,0 +1,22 @@
+﻿set(packet_name "glog-0.7.1")
+
+if(NOT EXISTS ${CMAKE_SOURCE_DIR}/ThirdParty/${packet_name})
+  execute_process(
+    COMMAND ${CMAKE_COMMAND} -E tar xzf ${CMAKE_SOURCE_DIR}/ThirdParty/${packet_name}.zip
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/ThirdParty)
+endif()
+
+set(WITH_GTEST
+    OFF
+    CACHE BOOL "" FORCE)
+set(WITH_GFLAGS
+    OFF
+    CACHE BOOL "" FORCE)
+set(BUILD_TESTING
+    OFF
+    CACHE BOOL "" FORCE)
+set(BUILD_SHARED_LIBS
+    OFF
+    CACHE BOOL "Build shared libraries" FORCE)
+
+add_subdirectory(${CMAKE_SOURCE_DIR}/ThirdParty/${packet_name})
