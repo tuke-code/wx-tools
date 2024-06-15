68191a1f99fab23dcd1d69f4e7a4c2a455dbe625
x-tools-author <x-tools@outlook.com>
2024-06-15 15:53:06 +0800

    chore: backup code

--- Source/Page/InputControlBox.cpp
+++ Source/Page/InputControlBox.cpp
@@ -16,7 +16,6 @@
 InputControlBox::InputControlBox(wxWindow* parent)
     : wxStaticBoxSizer(wxHORIZONTAL, parent, wxT("Input Control"))
 {
-#if 1
     auto* sizer = new wxGridBagSizer(4, 4);
     Add(sizer, 0, wxEXPAND | wxALL, 0);
 
@@ -35,26 +34,5 @@ InputControlBox::InputControlBox(wxWindow* parent)
     sizer->Add(cycleInterval, wxGBPosition(0, 1), wxGBSpan(1, 1), wxEXPAND, 0);
     sizer->Add(formatText, wxGBPosition(1, 0), wxGBSpan(1, 1), wxALIGN_CENTER_VERTICAL | wxALL, 0);
     sizer->Add(formatComboBox, wxGBPosition(1, 1), wxGBSpan(1, 1), wxEXPAND, 0);
-    sizer->Add(buttonSizer, wxGBPosition(2, 0), wxGBSpan(1, 2), wxEXPAND, 0);
-#else
-    auto sizer = new wxGridSizer(3, 2, 4, 4);
-    Add(sizer, 1, wxEXPAND | wxALL, 0);
-
-    auto cycleText = new wxStaticText(GetStaticBox(), wxID_ANY, wxT("Cycle"));
-    auto cycleInterval = new wxComboBox(GetStaticBox(), wxID_ANY);
-    auto formatText = new wxStaticText(GetStaticBox(), wxID_ANY, wxT("Format"));
-    auto formatComboBox = new TextFormatComboBox(GetStaticBox());
-    auto settingsButton = new wxButton(GetStaticBox(), wxID_ANY, wxT("Settings"));
-    auto sendingButton = new wxButton(GetStaticBox(), wxID_ANY, wxT("Send"));
-
-    auto buttonSizer = new wxBoxSizer(wxHORIZONTAL);
-    buttonSizer->Add(settingsButton, 1, wxEXPAND | wxALL, 0);
-    buttonSizer->Add(sendingButton, 1, wxEXPAND | wxALL, 0);
-
-    sizer->Add(cycleText, 0, wxALIGN_CENTER_VERTICAL | wxALL, 0);
-    sizer->Add(cycleInterval, 0, wxEXPAND | wxALL, 0);
-    sizer->Add(formatText, 0, wxALIGN_CENTER_VERTICAL | wxALL, 0);
-    sizer->Add(formatComboBox, 0, wxEXPAND | wxALL, 0);
-    sizer->Add(buttonSizer, 0, wxEXPAND | wxALL, 0);
-#endif
+    sizer->Add(buttonSizer, wxGBPosition(2, 0), wxGBSpan(1, 2), wxEXPAND | wxALL, 0);
 }
