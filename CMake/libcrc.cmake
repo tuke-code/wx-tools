5f2674162a29fd471d5e971048c29d52d8947f83
x-tools-author <x-tools@outlook.com>
2024-06-21 20:42:55 +0800

    chore: backup code

---
+++ CMake/libcrc.cmake
@@ -0,0 +1,15 @@
+# https://github.com/lammertb/libcrc
+
+set(packet_name "libcrc-master")
+
+if(NOT EXISTS ${CMAKE_SOURCE_DIR}/ThirdParty/${packet_name})
+  execute_process(
+    COMMAND ${CMAKE_COMMAND} -E tar xzf ${CMAKE_SOURCE_DIR}/ThirdParty/${packet_name}.zip
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/ThirdParty)
+endif()
+
+file(GLOB LIBCRC_H ${CMAKE_SOURCE_DIR}/ThirdParty/${packet_name}/include/*.*)
+file(GLOB LIBCRC_C ${CMAKE_SOURCE_DIR}/ThirdParty/${packet_name}/src/*.*)
+
+include_directories(${CMAKE_SOURCE_DIR}/ThirdParty/${packet_name}/include)
+add_library(libcrc STATIC ${LIBCRC_H} ${LIBCRC_C})
