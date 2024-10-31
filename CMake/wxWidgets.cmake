0596d16a34cc612f63144bc3af0c17721f25a1d5
x-tools-author <x-tools@outlook.com>
2024-10-31 22:40:27 +0800

    chore: update 3rd files

--- CMake/wxWidgets.cmake
+++ CMake/wxWidgets.cmake
@@ -1,4 +1,4 @@
-﻿set(packet_name "wxWidgets-3.2.5")
+﻿set(packet_name "wxWidgets-3.2.6")
 
 execute_process(COMMAND ${CMAKE_COMMAND} -E make_directory ${packet_name}
                 WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/ThirdParty)
