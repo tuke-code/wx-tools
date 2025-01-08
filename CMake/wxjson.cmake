f77bb4fa0077cbaa642c148b3846d275a0b40636
wuhai <wh@s-track.cn>
2025-01-08 18:45:00 +0800

    chore: update the file

--- CMake/wxjson.cmake
+++ CMake/wxjson.cmake
@@ -1,4 +1,4 @@
-# https://github.com/catalinr/wxjson.git
+# https://sourceforge.net/projects/wxcode/files/Components/ https://github.com/catalinr/wxjson.git
 set(packet_name "wxjson-master")
 
 if(NOT EXISTS ${CMAKE_SOURCE_DIR}/ThirdParty/${packet_name})
