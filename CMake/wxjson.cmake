389cd640af5fb172fec32352806c45a816556fc1
wuhai <wh@s-track.cn>
2025-01-08 18:56:49 +0800

    chore: update the file

--- CMake/wxjson.cmake
+++ CMake/wxjson.cmake
@@ -1,4 +1,6 @@
-# https://sourceforge.net/projects/wxcode/files/Components/ https://github.com/catalinr/wxjson.git
+# * https://sourceforge.net/projects/wxcode/files/Components/
+# * https://github.com/catalinr/wxjson.git
+# * 需要将jsonreader.cpp文件中的union byte定义屏蔽
 set(packet_name "wxjson-master")
 
 if(NOT EXISTS ${CMAKE_SOURCE_DIR}/ThirdParty/${packet_name})
