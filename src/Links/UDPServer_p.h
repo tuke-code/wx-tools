1c6b48a1feb4f7bb6d4f8a18a56178b4acf8ebed
x-tools-author <x-tools@outlook.com>
2025-01-20 18:24:20 +0800

    chore: update the file

--- src/Links/UDPServer_p.h
+++ src/Links/UDPServer_p.h
@@ -53,22 +53,6 @@ static void OnMgEvOpen(struct mg_connection *c, void *ev_data, UDPServer *q)
     wxtInfo() << fmt::format("UDP client connected from {0} to {1}", from, to);
 }
 
-static void OnMgEvResolve(struct mg_connection *c, void *ev_data, UDPServer *q)
-{
-    // c->rem gets populated with multicast address. Store it in c->data
-    memcpy(c->data, &c->rem, sizeof(c->rem));
-
-    auto *d = q->GetD<UDPServerPrivate>();
-    const std::string remIp = d->mg_addr_to_ipv4(&c->rem);
-    const uint16_t remPort = DoReverseByteOrder<uint16_t>(c->rem.port);
-    const std::string locIp = d->mg_addr_to_ipv4(&c->loc);
-    const uint16_t locPort = DoReverseByteOrder<uint16_t>(c->loc.port);
-    const std::string from = DoEncodeFlag(remIp, remPort);
-    const std::string to = DoEncodeFlag(locIp, locPort);
-
-    wxtInfo() << fmt::format("UDP client resolve from {0} to {1}", from, to);
-}
-
 static void OnMgEvRead(struct mg_connection *c, void *ev_data, UDPServer *q)
 {
     if (c->recv.len <= 0) {
@@ -82,6 +66,14 @@ static void OnMgEvRead(struct mg_connection *c, void *ev_data, UDPServer *q)
     const std::string ip = d->mg_addr_to_ipv4(&c->rem);
     const uint16_t port = DoReverseByteOrder<uint16_t>(c->rem.port);
     const std::string from = DoEncodeFlag(ip, port);
+
+    auto client = std::make_pair(ip, port);
+    auto clients = d->clients;
+    if (std::find(clients.begin(), clients.end(), client) == clients.end()) {
+        d->clients.push_back(client);
+        q->newClientSignal(ip, port);
+    }
+
     q->bytesRxSignal(std::move(bytes), c->recv.len, from);
 }
 
@@ -90,8 +82,6 @@ static void UDPServerHandler(struct mg_connection *c, int ev, void *ev_data)
     auto *q = reinterpret_cast<UDPServer *>(c->mgr->userdata);
     if (ev == MG_EV_OPEN) {
         OnMgEvOpen(c, ev_data, q);
-    } else if (ev == MG_EV_RESOLVE) {
-        OnMgEvResolve(c, ev_data, q);
     } else if (ev == MG_EV_READ) {
         OnMgEvRead(c, ev_data, q);
     } else if (ev == MG_EV_POLL) {
