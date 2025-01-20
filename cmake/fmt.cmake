774aa4f42968e5a0ee708de93ea7926e1397a5f2
x-tools-author <x-tools@outlook.com>
2025-01-20 16:57:35 +0800

    chore: add the file to project

---
+++ cmake/fmt.cmake
@@ -0,0 +1,9 @@
+﻿set(packet_name "fmt-11.1.2")
+
+if(NOT EXISTS ${CMAKE_SOURCE_DIR}/3rd/${packet_name})
+  execute_process(COMMAND ${CMAKE_COMMAND} -E tar xzf ${CMAKE_SOURCE_DIR}/3rd/${packet_name}.zip
+                  WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/3rd)
+endif()
+
+include_directories(${CMAKE_SOURCE_DIR}/3rd/${packet_name}/include)
+add_subdirectory(${CMAKE_SOURCE_DIR}/3rd/${packet_name})
