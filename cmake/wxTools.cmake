129d119e55fc25ee7959635a9706475b68719ee5
x-tools-author <x-tools@outlook.com>
2025-01-24 17:35:58 +0800

    chore: add the file to project

---
+++ cmake/wxTools.cmake
@@ -0,0 +1,15 @@
+﻿set(WX_TOOLS_BINARY_DIR ${CMAKE_BINARY_DIR}/assets)
+
+function(wxt_make_zip target)
+  if(WIN32)
+    string(TOLOWER ${target} lower_target)
+    string(TOLOWER ${CMAKE_HOST_SYSTEM_NAME} lower_system_name)
+    string(TOLOWER ${CMAKE_SYSTEM_PROCESSOR} lower_system_processor)
+    set(TAR_FILE_NAME ${lower_target}-${lower_system_name}-${lower_system_processor})
+    add_custom_command(
+      TARGET ${target}
+      POST_BUILD
+      COMMAND ${CMAKE_COMMAND} -E tar "cf" ${TAR_FILE_NAME}.zip "--format=zip" ${target}
+      WORKING_DIRECTORY ${WX_TOOLS_BINARY_DIR})
+  endif()
+endfunction()
