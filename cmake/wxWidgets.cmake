22af826cd4b054af67a1aae9d453adc790134c04
x-tools-author <x-tools@outlook.com>
2025-02-11 18:22:02 +0800

    chore: update the file

--- cmake/wxWidgets.cmake
+++ cmake/wxWidgets.cmake
@@ -9,14 +9,3 @@ endif()
 
 add_subdirectory(${CMAKE_SOURCE_DIR}/3rd/${packet_name})
 include_directories(${CMAKE_SOURCE_DIR}/3rd/${packet_name}/include)
-
-if(FALSE)
-  if(WIN32 AND WXT_RELEASE)
-    set(include_temp ${CMAKE_BINARY_DIR}/3rd/${packet_name}/lib/vc_lib/mswu)
-  else()
-    set(include_temp ${CMAKE_BINARY_DIR}/3rd/${packet_name}/lib/vc_lib/mswud)
-  endif()
-
-  message(STATUS "include_temp:${include_temp}")
-  include_directories(${include_temp})
-endif()
