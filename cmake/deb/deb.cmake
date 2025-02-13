445eb5888f9a15928661380bcc4c1bf9eabda803
x-tools-author <x-tools@outlook.com>
2025-02-13 20:44:07 +0800

    chore: add the file to project

---
+++ cmake/deb/deb.cmake
@@ -0,0 +1,20 @@
+function(wxt_make_deb target packetName friendlyName version)
+  file(GLOB_RECURSE RES_FILES ${CMAKE_SOURCE_DIR}/.cmake/deb/*)
+
+  set(args "-DargTarget=${target}")
+  list(APPEND args "-DargPacketName=${packetName}")
+  list(APPEND args "-DargFriendlyName=${friendlyName}")
+  list(APPEND args "-DargVersion=${version}")
+  list(APPEND args "-DargWorkingDir=${CMAKE_BINARY_DIR}/deb/${target}")
+
+  add_custom_target(
+    ${target}-deb
+    COMMAND ${CMAKE_COMMAND} -E remove_directory {CMAKE_BINARY_DIR}/deb/${target} "||"
+            ${CMAKE_COMMAND} -E true
+    COMMAND ${CMAKE_COMMAND} -E copy_directory ${CMAKE_SOURCE_DIR}/.cmake/deb/app
+            ${CMAKE_BINARY_DIR}/deb/${target} "||" ${CMAKE_COMMAND} -E true
+    COMMAND ${CMAKE_COMMAND} -E copy_directory ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}
+            ${CMAKE_BINARY_DIR}/deb/${target}/opt/${packetName}
+    COMMAND ${CMAKE_COMMAND} ${args} -P ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/deb_maker.cmake
+    SOURCES ${RES_FILES})
+endfunction()
