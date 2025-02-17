a5b29855699d666566cad3faefef71d884fe45e3
x-tools-author <x-tools@outlook.com>
2025-02-17 17:10:44 +0800

    chore: update the file

--- cmake/wxTools.cmake
+++ cmake/wxTools.cmake
@@ -6,9 +6,9 @@ function(wxt_make_zip target)
     string(TOLOWER ${CMAKE_HOST_SYSTEM_NAME} lower_system_name)
     # string(TOLOWER ${CMAKE_SYSTEM_PROCESSOR} lower_system_processor)
     if(CMAKE_SIZEOF_VOID_P EQUAL 8)
-      set(TAR_FILE_NAME ${lower_target}-${lower_system_name}-x64)
+      set(TAR_FILE_NAME ${lower_target}-${lower_system_name}-${WXT_GIT_TAG}-x64)
     else()
-      set(TAR_FILE_NAME ${lower_target}-${lower_system_name}-x86)
+      set(TAR_FILE_NAME ${lower_target}-${lower_system_name}-${WXT_GIT_TAG}-x86)
     endif()
     add_custom_command(
       TARGET ${target}
