8cc3c1e30729f81d10f88af257843711c1663287
x-tools-author <x-tools@outlook.com>
2025-02-17 23:59:33 +0800

    chore: update the file

--- scripts/msix/make-msix.bat
+++ scripts/msix/make-msix.bat
@@ -1,6 +1,5 @@
 mkdir build
 cd build
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
-cmake -DCMAKE_BUILD_TYPE=Release -DWXT_PORTABLE_EDITION:BOOL=OFF -G "NMake Makefiles" ../../../
-cmake clean
+cmake -DCMAKE_BUILD_TYPE=Release -DWXT_PORTABLE_EDITION:BOOL=OFF -G "Ninja" ../../../
 cmake --build . --target %1_msix
