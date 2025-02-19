933076738bfe015258ff0cc46a296ba1c2836585
x-tools-author <x-tools@outlook.com>
2025-02-19 12:04:14 +0800

    chore: update the file

--- cmake/Deb/deb.cmake
+++ cmake/Deb/deb.cmake
@@ -1,5 +1,5 @@
 function(wxt_make_deb target packetName friendlyName version)
-  file(GLOB_RECURSE RES_FILES ${CMAKE_SOURCE_DIR}/.cmake/deb/*)
+  file(GLOB_RECURSE RES_FILES ${CMAKE_SOURCE_DIR}/cmake/deb/*)
 
   set(args "-DargTarget=${target}")
   list(APPEND args "-DargPacketName=${packetName}")
@@ -7,14 +7,13 @@ function(wxt_make_deb target packetName friendlyName version)
   list(APPEND args "-DargVersion=${version}")
   list(APPEND args "-DargWorkingDir=${CMAKE_BINARY_DIR}/deb/${target}")
 
-  add_custom_target(
-    ${target}-deb
-    COMMAND ${CMAKE_COMMAND} -E remove_directory {CMAKE_BINARY_DIR}/deb/${target} "||"
-            ${CMAKE_COMMAND} -E true
-    COMMAND ${CMAKE_COMMAND} -E copy_directory ${CMAKE_SOURCE_DIR}/.cmake/deb/app
-            ${CMAKE_BINARY_DIR}/deb/${target} "||" ${CMAKE_COMMAND} -E true
-    COMMAND ${CMAKE_COMMAND} -E copy_directory ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}
-            ${CMAKE_BINARY_DIR}/deb/${target}/opt/${packetName}
+  # cmake-format: off
+  add_custom_target(${target}-deb
+    COMMAND ${CMAKE_COMMAND} -E remove_directory {CMAKE_BINARY_DIR}/deb/${target} "||" ${CMAKE_COMMAND} -E true
+    COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_BINARY_DIR}/deb/${target} "||" ${CMAKE_COMMAND} -E true
+    COMMAND ${CMAKE_COMMAND} -E copy_directory ${CMAKE_SOURCE_DIR}/cmake/Deb/app ${CMAKE_BINARY_DIR}/deb/${target} "||" ${CMAKE_COMMAND} -E true
+    COMMAND ${CMAKE_COMMAND} -E copy_directory ${CMAKE_RUNTIME_OUTPUT_DIRECTORY} ${CMAKE_BINARY_DIR}/deb/${target}/opt/${packetName}
     COMMAND ${CMAKE_COMMAND} ${args} -P ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/deb_maker.cmake
     SOURCES ${RES_FILES})
+  # cmake-format: on
 endfunction()
